544012475889c66488fa02ddcb8bfe30ae84aa1e
Aleksandr Chernishov <caa190993@gmail.com>
2021-05-20 15:43:11 +0300

    Update API.js

--- src/servises/API.js
+++ src/servises/API.js
@@ -3,33 +3,39 @@ import axios from 'axios';
 const BASE_URL = 'https://swapi.dev/api';
 
 
+
 export async function requestCharacters(page = 1) {
-  const { data } = await axios.get(`${BASE_URL}/people?page=${page}`);
+  const { data } = await axios.get(`${BASE_URL}/people?page=${page}`.replace('http:', 'https:'), {
+      headers: {
+        location: `${BASE_URL}/people?page=${page}`.replace('http:', 'https:')
+      }
+    }
+  );
   return data;
 }
 
 export async function requestPlanets(page = 1) {
-  const { data } = await axios.get(`${BASE_URL}/planets?page=${page}`);
+  const { data } = await axios.get(`${BASE_URL}/planets?page=${page}`.replace('http:', 'https:'));
   return data;
 }
 
 export async function requestHomeWorld(url) {
-    const { data } = await axios.get(`${url}`);
+    const { data } = await axios.get(`${url}`.replace('http:', 'https:'));
   return data;
 }
 
 export async function requestFilms(url) {
-    const { data } = await axios.get(`${url}`);
+    const { data } = await axios.get(`${url}`.replace('http:', 'https:'));
   return data;
 }
 
 export async function requestVehicles (url) {
-    const { data } = await axios.get(`${url}`);
+    const { data } = await axios.get(`${url}`.replace('http:', 'https:'));
   return data;
 }
 
 export async function requestFavorites(url) {
-    const { data } = await axios.get(`${url}`);
+    const { data } = await axios.get(`${url}`.replace('http:', 'https:'));
   return data;
 }
 
